2b83796d5aa77318800e7615a3b208bbc22d6094
jitesh9372 <jiteshkanojiya6@gmail.com>
2026-04-15 08:30:01 +0530

    live map

--- src/pages/LiveMap.tsx
+++ src/pages/LiveMap.tsx
@@ -174,10 +174,10 @@ export default function LiveMap({ activeAlertId, location }: LiveMapProps) {
         >
           <MapController center={center} />
           
-          {/* Dark map tiles matching the previous Google Maps styling as closely as possible */}
+          {/* Bright and highly visible modern map styling */}
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           />
 
           {/* User Location Marker */}
